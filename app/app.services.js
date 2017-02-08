e171b8ae67d8efe3eac7369e923466757579aeb8
ilomon10 <ilomon10@gmail.com>
2017-02-08 16:06:20 +0800

    Update app.services.js

--- app/app.services.js
+++ app/app.services.js
@@ -26,7 +26,7 @@ function logger($log) {
 	vm.error = function (msg) { $log.error(msg); }
 	vm.debug = function (msg) { $log.debug(msg); }
 }
-var baseURL = 'http://10.11.12.242/belajar/backend/index.php/api';
+// var baseURL = 'http://10.11.12.242/belajar/backend/index.php/api';
 var baseURL = 'http://edgarjeremy.com/belajar/backend/index.php/api';
 // var baseURL = 'http://localhost/belajar/backend/index.php/api';
 
